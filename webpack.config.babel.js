313a8ed16c3dd0774ec41e36e88b35f73afca1da
Renan Caldas <renan.caldas@outlook.com>
2017-02-20 15:31:34 -0300

    Trying yo inject bundle into html (in development)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,37 +1,31 @@
-var path = require('path')
-var webpack = require('webpack')
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var HTMLWebpackPlugin = require('html-webpack-plugin')
+import path from 'path';
+import webpack from 'webpack';
+import OpenBrowserPlugin from 'open-browser-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const DEV_SERVER_URL = 'http://localhost:8080';
 const DEVELOPMENT = process.env.NODE_ENV === 'development'
 const PRODUCTION = process.env.NODE_ENV === 'production'
 
-console.log('Building for: ', PRODUCTION ? 'PRODUCTION' : 'DEVELOPMENT');
 
-var entry = PRODUCTION
-    ? ['./src/App.jsx']
-    : [
-        './src/App.jsx',
-        'webpack/hot/dev-server',
-        'webpack-dev-server/client?' + DEV_SERVER_URL
-      ]
+console.log('============================ WhitePrompt ============================');
+console.log('Webpack is building for: ', PRODUCTION ? 'PRODUCTION' : 'DEVELOPMENT');
 
-var plugins = PRODUCTION
+
+let plugins = PRODUCTION
     ? [
         new webpack.optimize.UglifyJsPlugin(),
-        new HTMLWebpackPlugin({ template: 'index-template.html' })
+        new HtmlWebpackPlugin({ template: './index.html' })
       ]
     : [
-        new HTMLWebpackPlugin({ template: 'index.html' }),
         new webpack.HotModuleReplacementPlugin(),
-        new OpenBrowserPlugin({ url: DEV_SERVER_URL })
+        new OpenBrowserPlugin({ url: DEV_SERVER_URL }),
+        new HtmlWebpackPlugin({ template: './index.html' })
       ]
 
 // Injecting global variables into source code
 plugins.push(
     new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV.toLowerCase()) },
         DEVELOPMENT: JSON.stringify(DEVELOPMENT),
         PRODUCTION: JSON.stringify(PRODUCTION)
     })
@@ -39,11 +33,11 @@ plugins.push(
 
 module.exports = {
     devtool: 'source-map', // to see the actual es6 code in chrome dev tools
-    entry: entry,
+    entry: './src/App.jsx',
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: PRODUCTION ? '/' : '/dist/',
-        filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
+        filename:  PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
     },
     module: {
         loaders: [{
